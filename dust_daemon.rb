0a55ad02d284af2befeb8667312a96f53ddbd312
Alexander <ati@MacBook.local>
2012-08-20 01:03:20 +0400

    added output example

--- dust_daemon.rb
+++ dust_daemon.rb
@@ -2,16 +2,18 @@ require 'sequel'
 require 'serialport'
 
 XBEE_PORT_DEV = "/dev/tty.usbserial-AH01DH9Y"
-XBEE_PORT_PARAMS = {'baud' => 9600, 'data_bits' => 8, 'stop_bits' => 1, 'parity' => SerialPort::NONE}
+#XBEE_PORT_DEV = "/dev/tty.PL2303-00001004"
+XBEE_PORT_PARAMS = {'baud' => 38400, 'data_bits' => 8, 'stop_bits' => 1, 'parity' => SerialPort::NONE}
 
 def setup
 
 end
-
+#"{\"d1\":5989,\"d2\":136,\"t1\":25,\"h1\":58,\"rc\":1,\"pc\":96,\"ts\":194250,\"hs\":0}\r\n"}
 def main_loop
     SerialPort.open(XBEE_PORT_DEV, XBEE_PORT_PARAMS) { |sp|
         while sensors = sp.gets
-            p sensors.strip
+            #p sensors.strip
+            p sensors
         end
     }
 end
