94babefe51208b448c0d8bf98d6baefe2c5280e7
Alexander <variomap@gmail.com>
2012-08-21 15:52:23 +0400

    config file changes

--- dust_daemon.rb
+++ dust_daemon.rb
@@ -2,11 +2,13 @@ require 'serialport'
 require 'net/http'
 require 'parseconfig'
 
-XBEE_PORT_DEV = "/dev/tty.usbserial-AH01DH9Y"
+#XBEE_PORT_DEV = "/dev/tty.usbserial-AH01DH9Y"
 #XBEE_PORT_DEV = "/dev/tty.PL2303-00001004"
+
 XBEE_PORT_PARAMS = {'baud' => 38400, 'data_bits' => 8, 'stop_bits' => 1, 'parity' => SerialPort::NONE}
 
-@config = ParseConfig.new('./db/dust.config')
+@config = ParseConfig.new(File.dirname(__FILE__) + '/db/dust.config')
+STDOUT.sync = true
 
 def send_data(dc1100_string)
     uri = URI(@config['data_url'])
@@ -20,13 +22,13 @@ def send_data(dc1100_string)
 
     if res.is_a?(Net::HTTPSuccess)
     else
-        puts "#{Time.now.to_s}: ERROR sending request to #{DATA_URL}: #{res.inspect}"
+        puts "#{Time.now.to_s}: ERROR sending request to #{@config['data_url']}: #{res.inspect}"
     end
 end
 
 #{"d1":5989,"d2":136,"t1":25,"h1":58,"rc":1,"pc":96,"ts":194250,"hs":0}
 def main_loop
-    SerialPort.open(XBEE_PORT_DEV, XBEE_PORT_PARAMS) { |sp|
+    SerialPort.open(@config['xbee_device'], XBEE_PORT_PARAMS) { |sp|
         while sensors = sp.gets
             send_data(sensors.strip)
             puts "#{Time.now.to_s}: #{sensors}"
