61b458a2bbe9ef7a645663b1cf5cca1b4cb27ab7
Alexander <ati@undev.ru>
2013-09-25 14:07:28 +0000

    moved db writing to separate handle

--- script/dust_stats.r
+++ script/dust_stats.r
@@ -8,12 +8,12 @@ averages = c("select avg(d1) as d from dc1100s group by measured_at/3600", "sele
 trends = c("select measured_at, d1 as d from dc1100s order by id desc limit 300", "select measured_at, d2 as d from dc1100s order by id desc limit 300")
 sql = data.frame(averages, trends)
 
-dbh = dbConnect(SQLite(), db_name)
+dbh_read = dbConnect(SQLite(), db_name, flags = SQLITE_RO)
 
 get_data <- function(q_type, n_sensor)
 {
   query = paste(sql[[q_type]][n_sensor])
-	res = dbGetQuery(dbh, query)
+	res = dbGetQuery(dbh_read, query)
 }
 
 get_quantiles <- function(n_sensor)
@@ -29,11 +29,19 @@ get_trend <- function(n_sensor)
   paste(round(r[[1]][2]))
 }
 
-# main loop
-for (sensor in 1:2)
-{
-  t = get_trend(sensor)
-  q = get_quantiles(sensor)
-  res = dbSendQuery(dbh, paste("update dc1100s_stats set trend=", t, ", quantiles='", q, "' where n_sensor=", sensor, sep=""))
-  dbClearResult(res)
-}
+
+t1 = get_trend(1)
+q1 = get_quantiles(1)
+
+t2 = get_trend(2)
+q2 = get_quantiles(2)
+
+dbDisconnect(dbh_read)
+
+dbh_write = dbConnect(SQLite(), db_name)
+res = dbSendQuery(dbh_write, paste("update dc1100s_stats set trend=", t1, ", quantiles='", q1, "' where n_sensor=1", sep=""))
+dbClearResult(res)
+res = dbSendQuery(dbh_write, paste("update dc1100s_stats set trend=", t2, ", quantiles='", q2, "' where n_sensor=2", sep=""))
+dbClearResult(res)
+
+dbDisconnect(dbh_write)
