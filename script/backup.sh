87aba9929cbd2102b7866a635ab8da12a272ffbf
Alexander <ati@undev.ru>
2014-11-26 08:58:32 +0000

    create backups in tar format

--- script/backup.sh
+++ script/backup.sh
@@ -3,5 +3,5 @@
 BACKUP_DIR=/var/data/backup/vozduh
 
 /usr/bin/rsync -r /home/ati/Dropbox/vozduh $BACKUP_DIR/potd/
-/usr/bin/pg_dump -U vozduh vozduh | /bin/gzip > $BACKUP_DIR/db/$(/bin/date +%Y-%m-%d-db-vozduh.gz)
+/usr/bin/pg_dump --format=t -U vozduh vozduh | /bin/gzip > $BACKUP_DIR/db/$(/bin/date +%Y-%m-%d-db-vozduh.gz)
 /usr/bin/find $BACKUP_DIR/db -mtime +7 -exec /bin/rm {} \;
